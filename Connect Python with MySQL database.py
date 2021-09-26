20d6834146a6801bc9c06f0c55dce1858a410c78
Vishwajeet Deshmukh <69856637+MrVishwajeetDeshmukh@users.noreply.github.com>
2021-09-26 18:02:23 +0530

    Update Connect Python with MySQL database.py

--- Connect Python with MySQL database.py
+++ Connect Python with MySQL database.py
@@ -3,44 +3,61 @@ import mysql.connector
 mydb=mysql.connector.connect(
     host='localhost', 
     user='root', 
-    password='*********',  #put your MySQL password
+    password='123456789',  #put your MySQL password
     database='db1')
 print(mydb.connection_id)
 
 cur=mydb.cursor()
-#To create database
-#S="CREATE TABLE book(bookid integer(4),tital varchar(20),price float(5,2))"
-#cur.execute(S)
 
-#To insert table
-#s="INSERT INTO book  (bookid,tital,price)  VALUES (%s, %s, %s)"
 
-#to insert single input
-#b1=(1, 'Python3',345)
-#cur.execute(s,b1)        #  cur.execute()  it will make changes in database
-#mydb.commit()            #  mydb.commit()  is used to save permenent chabges
+'''To create database'''
+S="CREATE TABLE book (bookid integer(4),title varchar(20),price float(5,2))"
+cur.execute(S)
 
-#To insert multiple input
-#books=[(2,'PHP',135),(3,'JAVA',40),(4,'HTML',200)]
-#cur.executemany(s,books)
-#mydb.commit()
+'''To insert table'''
+s="INSERT INTO book  (bookid,title,price)  VALUES (%s, %s, %s)"
 
-#to fetch content of table
+'''to insert single input'''
+# bookid=int(input("Enter book id:-"))
+# title=(input("Enter book title:-"))
+# price=int(input("Enter book price:-"))
+# b1=(bookid, title ,price)
+# cur.execute(s,b1)        #  cur.execute()  it will make changes in database
+# mydb.commit()            #  mydb.commit()  is used to save permenent chabges
+
+'''To insert multiple input'''
+n=int(input("Enter no. of input :-"))
+book=[]
+for i in range (n):
+    bookid=int(input("Enter book id:-"))
+    title=(input("Enter book title:-"))
+    price=int(input("Enter book price:-"))
+    book.append((bookid, title, price))
+
+
+# book=[(2,'PHP',135),(3,'JAVA',40),(4,'HTML',200)]
+cur.executemany(s,book)
+mydb.commit()
+
+'''to fetch content of table'''
 #s="SELECT * from book"
 #cur.execute(s)
 #result=cur.fetchall()
 #for rec in result:
 #    print(rec)
 
-#to update table
+'''to update table'''
 #s="UPDATE book SET price=price+10 WHERE price>200"
 #cur.execute(s)
 #mydb.commit()  
 
-#to delete total table
+'''to delete total table'''
 #s="DELETE form book"
 
-#to delete perticular item
-#s="DELETE from book WHERE tital='PHP' "
+'''to delete perticular item'''
+#s="DELETE from book WHERE title='PHP' "
 #cur.execute(s)
 #mydb.commit
+
+'''To delete database'''
+#s="DROP database db1"
